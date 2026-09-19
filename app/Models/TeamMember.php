49a3c937b9b619822dc97e34fe23c117a08802a7
manucharyansos <88939484+manucharyansos@users.noreply.github.com>
2026-09-19 18:03:54 +0400

    Add managed team members

---
+++ app/Models/TeamMember.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class TeamMember extends Model
+{
+    protected $fillable = [
+        'slug', 'status', 'show_on_homepage', 'sort_order', 'translations', 'images',
+    ];
+
+    protected function casts(): array
+    {
+        return [
+            'show_on_homepage' => 'boolean',
+            'translations' => 'array',
+            'images' => 'array',
+        ];
+    }
+}

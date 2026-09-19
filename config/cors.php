666de761ecc1ccd9f455e2b6f1c046a965edb967
manucharyansos <88939484+manucharyansos@users.noreply.github.com>
2026-09-19 18:29:59 +0400

    Allow preview frontend origin for API requests

--- config/cors.php
+++ config/cors.php
@@ -1,9 +1,16 @@
 <?php
 
+$configuredOrigins = array_filter(array_map('trim', explode(',', env('FRONTEND_URL', ''))));
+
 return [
     'paths' => ['api/*'],
     'allowed_methods' => ['*'],
-    'allowed_origins' => array_filter(array_map('trim', explode(',', env('FRONTEND_URL', 'http://localhost:5173')))),
+    'allowed_origins' => array_values(array_unique(array_merge([
+        'https://abcn.am',
+        'https://www.abcn.am',
+        'https://preview.abcn.am',
+        'http://localhost:5173',
+    ], $configuredOrigins))),
     'allowed_origins_patterns' => [],
     'allowed_headers' => ['*'],
     'exposed_headers' => [],
